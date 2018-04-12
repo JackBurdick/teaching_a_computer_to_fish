09c5c25d8a104c15968b034d38f09b75917e4244
Jack Burdick <jackbburdick@gmail.com>
2018-04-12 18:33:05 -0400

    Update sync script to include output option
    1) also, update style for python in/out
    2) this script it messy, I'll need to refactor this.. but it is currently working as expected for my usecase

--- latex_guidebook/util_scripts/sync_code_from_table.py
+++ latex_guidebook/util_scripts/sync_code_from_table.py
@@ -2,7 +2,7 @@ import json
 # sync python files
 
 
-def create_latex_entry(code_list, language="python"):
+def create_latex_entry(code_list, language="pyInStyle"):
     """Creates a python-like code block for latex
     
     Args:
@@ -13,23 +13,20 @@ def create_latex_entry(code_list, language="python"):
         empty if no code was passed in.
     """
     code_block = []
-    if language == "python":
-        code_block.append("\\begin{lstlisting}[language=Python]\n")
-    else:
-        code_block.append("\\begin{lstlisting}[language=Bash]\n")
+    code_block.append("\\begin{lstlisting}[style="+language+"]\n")
     code_block.extend(code_list)
     code_block.append("\\end{lstlisting}\n")
     
     return code_block
 
-def create_new_tex_file_data(sync_id, l_path, opts, code_block):
-    """write the formatted latex block to the .tex file
+def create_new_tex_file_data(sync_id, l_path, code_block, language="pyInStyle"):
+    """create the formatted data including the latex block to write to the .tex file
     
     Args:
         sync_id: the id of the code block being synched
         l_path: String path to the tex file
-        opts: any formatting options
         code_block: the block of code to write to the file
+        language: the language to specify for latex
 
     Returns:
         data: "correctly" created latex data
@@ -40,31 +37,37 @@ def create_new_tex_file_data(sync_id, l_path, opts, code_block):
     with open(l_path, 'r') as fh:
         data = fh.readlines()
         idx = 0
+        prev_block_stop = -1
         for idx, line in enumerate(data):
             clean = line.strip()
             if clean == "% {{{" + str(sync_id) + "}}}":
-                code_idx = idx
+                # will insert code block on next line
+                code_idx = idx+1
                 break
 
         # handling a pre-existing code block vs new code block
-        if data[code_idx+1].strip() == "\\begin{lstlisting}[language=Python]":
+        if data[code_idx].strip() == "\\begin{lstlisting}[style="+language+"]":
             end_found = False
             for idx, line in enumerate(data[code_idx:]):
                 clean = line.strip()
                 if clean.strip() == "\\end{lstlisting}":
                     end_found = True
-                    prev_block_stop = idx
+                    prev_block_stop = idx+1
                     break
             
             if end_found:
-                data[code_idx+1: code_idx+1+prev_block_stop] = code_block
+                data[code_idx: code_idx+prev_block_stop] = code_block
             else:
                 print("ERROR!, no stop code found for code block {}".format(sync_id))
 
         else:
             # embed new code block
-            data[code_idx+1: code_idx+1] = code_block
-    return data
+            data[code_idx: code_idx] = code_block
+    
+    if prev_block_stop >= 0:
+        return data, code_idx+prev_block_stop
+    else:
+        return data, code_idx+len(code_block)
 
 
 def get_code_from_py(sync_id, c_path):
@@ -98,21 +101,65 @@ def get_cell_output(c_path, target_index):
     return target_output
 
 
+def include_output_block(sync_id, data, fmt_out_block, entry_point, language="pyOutStyle"):
+    """create the formatted data including the latex block to write to the .tex file
+    
+    Args:
+        sync_id: the id of the code block being synched
+        data: tex file data
+        fmt_out_block: the block of code to write to the file
+        language: the language to specify for latex
+
+    Returns:
+        data: "correctly" created latex data
+    """
+
+    # go to next line
+    entry_point += 1
+
+    # handling a pre-existing code block vs new code block
+    if data[entry_point].strip() == "\\begin{lstlisting}[style="+language+"]":
+        end_found = False
+        for idx, line in enumerate(data[entry_point+1:]):
+            clean = line.strip()
+            if clean.strip() == "\\end{lstlisting}":
+                end_found = True
+                block_stop_loc = idx+1
+                break
+        
+        if end_found:
+            data[entry_point: entry_point+block_stop_loc+1] = fmt_out_block
+        else:
+            print("ERROR!, no stop code found for code block {}".format(sync_id))
+
+    else:
+        # embed new code block
+        data[entry_point: entry_point] = fmt_out_block
+    return data
+
+
 def sync_snippet(sync_id, c_path, l_path, opts):
     code_strs, target_index = get_code_from_py(sync_id, c_path)
+
+    fmt_code_block = create_latex_entry(code_list=code_strs, language="pyInStyle")
+    new_data, prev_block_stop = create_new_tex_file_data(sync_id, l_path, 
+                                                         fmt_code_block, 
+                                                         language="pyInStyle")
+
+    # handle options
     opts = opts.split()
     if "o" in opts:
         if target_index >= 0:
             output_strs = get_cell_output(c_path, target_index)
             fmt_out_block = create_latex_entry(code_list=output_strs, 
-                                               language="bash")
-            print(fmt_out_block)
+                                               language="pyOutStyle")
+            new_data = include_output_block(sync_id, new_data, fmt_out_block, 
+                                            prev_block_stop, language="pyOutStyle")
         else:
             print("Error: no ouptput for indicated cell")
 
-    fmt_code_block = create_latex_entry(code_list=code_strs, language="python")
-    
-    new_data = create_new_tex_file_data(sync_id, l_path, opts, fmt_code_block)
+    #print(new_data)
+    # ===== write to file
     if new_data:
         # Write to file: overwrite existing file with modifications
         with open(l_path, 'w') as fh:
