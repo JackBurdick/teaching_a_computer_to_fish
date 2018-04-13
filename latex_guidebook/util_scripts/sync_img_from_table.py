4a418055cb6cfa3d8c62c3698dac53e80e011620
Jack Burdick <jackbburdick@gmail.com>
2018-04-13 12:00:20 -0400

    include latex entry code and option for fig width
    update tex files in git

--- latex_guidebook/util_scripts/sync_img_from_table.py
+++ latex_guidebook/util_scripts/sync_img_from_table.py
@@ -62,7 +62,79 @@ def create_image_path(sync_id):
         img_path = os.path.join(img_path, id_split[-1] + IMG_ext)
     
     return img_path
- 
+
+def create_latex_fig_entry(img_path, sync_id, width=0.6, caption=None):
+    """Creates a figure block for latex
+    
+    Args:
+        img_path: path to image file
+            
+    Returns:
+        Latex figure code lines. The code block will be empty if no code was 
+        passed in.
+    """
+
+    code_block = []
+    code_block.append("\\begin{figure}\n")
+    code_block.append("\\centering\n")
+    code_block.append("\\includegraphics[width=" + width + "\\textwidth]{" + img_path + "}\n")
+    if caption:
+        code_block.append("\\caption{" + caption + "}\n")
+    code_block.append("\\label{fig:" + sync_id + "}\n")
+    code_block.append("\\end{figure}\n")
+
+    return code_block
+
+def create_new_tex_file_data(sync_id, l_path, code_block):
+    """create the formatted data including the latex block to write to the .tex file
+    
+    Args:
+        sync_id: the id of the code block being synched
+        l_path: String path to the tex file
+        code_block: the block of code to write to the file
+        language: the language to specify for latex
+
+    Returns:
+        data: "correctly" created latex data
+    """
+
+    # TODO: consider restructuring this function
+
+    with open(l_path, 'r') as fh:
+        data = fh.readlines()
+        idx = 0
+        prev_block_stop = -1
+        for idx, line in enumerate(data):
+            clean = line.strip()
+            if clean == "% {{{" + str(sync_id) + "}}}":
+                # will insert code block on next line
+                code_idx = idx+1
+                break
+
+        # handling a pre-existing code block vs new code block
+        if data[code_idx].strip() == "\\begin{figure}":
+            end_found = False
+            for idx, line in enumerate(data[code_idx:]):
+                clean = line.strip()
+                if clean.strip() == "\\end{figure}":
+                    end_found = True
+                    prev_block_stop = idx+1
+                    break
+            
+            if end_found:
+                data[code_idx: code_idx+prev_block_stop] = code_block
+            else:
+                print("ERROR!, no stop code found for code block {}".format(sync_id))
+
+        else:
+            # embed new code block
+            data[code_idx: code_idx] = code_block
+    
+    if prev_block_stop >= 0:
+        return data, code_idx+prev_block_stop
+    else:
+        return data, code_idx+len(code_block)
+
 def sync_snippet(sync_id, c_path, l_path, opts):
 
     img_path = create_image_path(sync_id)
@@ -70,14 +142,32 @@ def sync_snippet(sync_id, c_path, l_path, opts):
     if target_index >= 0:
         img_str = get_img_str(c_path, target_index)
         success = convert_and_write(img_str, img_path)
-        if success:
-            print("Completed {}".format(sync_id))
-        else:
+        if not success:
             print("Error syncing {}".format(sync_id))
     else:
         print("Error: no img data for indicated cell")
-    #opts = opts.split()
-    #if "o" in opts:
+    
+    # handle options
+    #print(code_block)
+    img_w = 0.6
+    opts = opts.split()
+    for o in opts:
+        if o.startswith("width"):
+            img_w = o.split("-")[1]
+
+    # create latex information
+    # need to remove `.` from img path since we need to be relative to main.tex
+    # not this file anymore
+    img_path = img_path[1:]
+    code_block = create_latex_fig_entry(img_path, sync_id, width=img_w, caption=None)
+    new_data, _ = create_new_tex_file_data(sync_id, l_path, code_block)
+    
+    # ===== write to file
+    if new_data:
+        # Write to file: overwrite existing file with modifications
+        with open(l_path, 'w') as fh:
+            fh.writelines(new_data)
+        print("Completed: {}".format(sync_id))
 
 
 def read_sync_table(table_file):
@@ -87,9 +177,8 @@ def read_sync_table(table_file):
             sync_snippet(sync_id=cols[0], c_path=cols[1], 
                          l_path=cols[2], opts=cols[3])
 
-
 def main():
-    read_sync_table(table_file = "./img_table.txt")
+    read_sync_table(table_file = "./img_sync_table.txt")
 
 if __name__ == "__main__":
     main()
